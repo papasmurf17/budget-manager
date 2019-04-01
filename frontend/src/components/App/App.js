39404d8f0bddfda625bb1dfb843a71219ea507db
Matteo Codogno <matteo.codogno@welld.ch>
2019-04-01 22:34:33 +0200

    Add background color

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Heading from '@welld/react-components/lib/Heading';
 import Text from '@welld/react-components/lib/Text';
+
+import './App.scss';
+
 import Views from '../../views';
 import LoggedUser from '../LoggedUser';
 
 const App = () => (
   <div className='app h-screen flex flex-col'>
-    <div className='flex border-b border-grey-lighter px-10 py-4 items-center'>
+    <div className='flex border-b border-grey-light px-10 py-4 items-center'>
       <Heading size='h3' className='flex-1'>
         <Text weight='bold' component='span'>Budget</Text>
         Manager
       </Heading>
       <LoggedUser />
     </div>
-    <div className='flex-1'>
+    <div className='flex-1 p-10 page-container'>
       <Views />
     </div>
   </div>
