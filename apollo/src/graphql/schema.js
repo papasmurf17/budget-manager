af6086cc2f51605131c97e2946200156d48fbd15
Matteo Codogno <matteo.codogno@welld.ch>
2019-05-19 20:26:09 +0200

    Add the user data to context

--- apollo/src/graphql/schema.js
+++ apollo/src/graphql/schema.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server-express');
 const { importSchema } = require('graphql-import');
+const jwtDecode = require('jwt-decode');
+const debug = require('debug')('bm');
 
 const resolvers = require('./resolvers/transaction');
 
@@ -11,6 +13,22 @@ const typeDefs = importSchema('./src/graphql/schema.graphql');
 const apolloServerConfiguration = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => {
+    const token = req.headers.authorization;
+    if (token) {
+      const payload = jwtDecode(token);
+      const userData = {
+        username: payload.preferred_username,
+        firstName: payload.given_name,
+        lastName: payload.family_name,
+        email: payload.email
+      };
+      debug(`user: ${userData.username}`);
+      return { userData };
+    }
+    debug('No JWT token found in the request header');
+    return { userData: null };
+  },
   playground: {
     endpoint: 'http://localhost:4000/graphql',
     settings: {
