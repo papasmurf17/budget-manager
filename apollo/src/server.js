266f04232224fd9f44e1c82c0aaa35d09239e941
Matteo Codogno <matteo.codogno@welld.ch>
2019-08-25 15:44:12 +0200

    Refactor on express server

--- apollo/src/server.js
+++ apollo/src/server.js
@@ -4,18 +4,15 @@ const debug = require('debug')('bm');
 const apolloServerConfiguration = require('./graphql/schema');
 const mongoose = require('./config/mongoose');
 
-const graphQLServer = express();
 mongoose();
 
-apolloServerConfiguration.applyMiddleware({
-  app: graphQLServer
-});
+const app = express();
+apolloServerConfiguration.applyMiddleware({ app });
 
 const PORT = process.env.PORT || 4000;
-
-graphQLServer.listen(PORT, () => {
+app.listen(PORT, () => {
   debug(`🚀 The server has started on port: ${PORT}`);
   debug(`http://localhost:${PORT}/graphql`);
 });
 
-module.exports = graphQLServer;
+module.exports = app;
