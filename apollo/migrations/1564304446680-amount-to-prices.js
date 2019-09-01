98373333c01a1c3d6ef6cec5ab72e7a8b54fc071
Matteo Codogno <matteo.codogno@welld.ch>
2019-09-01 18:43:50 +0200

    Fix Price migration

--- apollo/migrations/1564304446680-amount-to-prices.js
+++ apollo/migrations/1564304446680-amount-to-prices.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 const { MongoClient } = require('mongodb');
 const Bluebird = require('bluebird');
 
@@ -8,24 +7,36 @@ const config = require('../src/config/config');
 
 exports.up = next => {
   let mClient = null;
+
   return MongoClient
     .connect(config.db.host, { useNewUrlParser: true })
     .then(client => {
       mClient = client;
       return client.db(config.db.name);
     })
-    .then(db => db.collection('transactions')
-      .find({ prices: { $exists: false } })
-      .forEach(transaction => {
-        if (!transaction) { return next('All transactions have prices fields') }
-        const { amount, currencyCode } = transaction;
-        transaction.pricePaid = { currency: currencyCode, value: amount };
-        transaction.priceConverted = {};
-        db.collection('transactions').save(transaction);
-      }))
+    .then(db => new Promise((resolve, reject) => {
+      db.collection('transactions')
+        .find({})
+        .map(transaction => {
+          if (!transaction) { return next('All transactions have prices fields') }
+          const { amount, currencyCode } = transaction;
+          console.log(`Updating ${transaction._id}`);
+          return db.collection('transactions').updateOne({
+            _id: transaction._id
+          }, { $set: {
+            'pricePaid': { 'currency': currencyCode, 'value': amount },
+            'priceConverted': { 'currency': currencyCode, 'value': amount },
+          } }, { upsert: true });
+        })
+        .toArray()
+        .then(promiseArray => Promise.all(promiseArray)
+          .then(() => resolve())
+          .catch(err => reject(err)))
+        .catch(err => reject(err));
+    }))
     .then(() => {
-      mClient.close();
       console.log('Amounts and currencies are migrated in the new prices structure!');
+      mClient.close();
       return next();
     })
     .catch(err => {
@@ -35,26 +46,4 @@ exports.up = next => {
 
 exports.down = next => {
   console.log('down');
-  let mClient = null;
-  return MongoClient
-    .connect(config.db.host, { useNewUrlParser: true })
-    .then(client => {
-      mClient = client;
-      return client.db(config.db.name);
-    })
-    .then(db => db.collection('transactions').updateMany(
-      {
-        amount: { $exists: true }
-      },
-      {
-        $unset: { amount: '' }
-      }
-    ))
-    .then(() => {
-      mClient.close();
-      return next();
-    })
-    .catch(err => {
-      throw err;
-    });
 };
