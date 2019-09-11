1e90d5f005db4876278333815352e26a484538b4
Matteo Codogno <matteo.codogno@welld.ch>
2019-09-11 23:20:23 +0200

    Add toolbar on transaction item

--- frontend/src/views/TransactionList/TransactionList.js
+++ frontend/src/views/TransactionList/TransactionList.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import distanceInWords from 'date-fns/distance_in_words';
 import Badge from '@welld/react-components/lib/Badge';
+import Button from '@welld/react-components/lib/Button';
+import ButtonGroup from '@welld/react-components/lib/ButtonGroup';
+import Icon from '@welld/react-components/lib/Icon';
 
 import Error from '../../components/Error';
 
@@ -17,11 +20,11 @@ const TransactionList = ({ transactions, history }) => (
             _id, invoiceDate, description, user, reporter, pricePaid, priceConverted, expenseType
           }) => (
             <div
-              onClick={() => history.push(`/transactions/edit/${_id}`)}
-              className='item padding-15 flex'
+              className={
+                classnames('item padding-15 flex', {
+                  active: history.location.pathname.endsWith(_id)
+                })}
               key={_id}
-              role='button'
-              tabIndex={0}
             >
               <div className='flex-1 m-l-15'>
                 <p className='recipients no-margin hint-text text-lg'>{reporter}</p>
@@ -55,6 +58,19 @@ const TransactionList = ({ transactions, history }) => (
                 <Badge type='edgy' color='warning' className='self-end'>
                   {expenseType}
                 </Badge>
+                <div className='flex self-end'>
+                  <ButtonGroup color='success' size='tiny'>
+                    <Button onClick={() => history.push(`/transactions/edit/${_id}`)}>
+                      <Icon name='pencil' />
+                    </Button>
+                    <Button onClick={() => console.log('delete')}>
+                      <Icon name='trash' />
+                    </Button>
+                    <Button onClick={() => console.log('clone')}>
+                      <Icon name='clone' />
+                    </Button>
+                  </ButtonGroup>
+                </div>
               </div>
             </div>
           ))
